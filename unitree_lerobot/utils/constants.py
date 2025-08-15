c41d3de1fea085dab9a0a58a15b81622b000c969
yucheon6000 <yucheon6000@gmail.com>
2025-08-15 17:58:04 +0900

    Update: constants for tactile evaluation

--- unitree_lerobot/utils/constants.py
+++ unitree_lerobot/utils/constants.py
@@ -1,14 +1,23 @@
 
 import dataclasses
+from collections import OrderedDict
 from typing import List, Dict
+
 
 @dataclasses.dataclass(frozen=True)
 class RobotConfig:
     motors: List[str]
     cameras: List[str]
-    camera_to_image_key:Dict[str, str]
+    camera_to_image_key: Dict[str, str]
     json_state_data_name: List[str]
     json_action_data_name: List[str]
+
+
+@dataclasses.dataclass(frozen=True)
+class TactileRobotConfig(RobotConfig):
+    tactiles: List[str]
+    # Maps tactile names to their image shapes (height, width)
+    tactile_to_image_shape: Dict[str, tuple]
 
 
 Z1_CONFIG = RobotConfig(
@@ -33,9 +42,10 @@ Z1_CONFIG = RobotConfig(
         "cam_left_wrist",
         "cam_right_wrist",
     ],
-    camera_to_image_key = {'color_0': 'cam_high', 'color_1': 'cam_left_wrist' ,'color_2': 'cam_right_wrist'},
-    json_state_data_name = ['left_arm', 'right_arm'],
-    json_action_data_name = ['left_arm', 'right_arm']
+    camera_to_image_key={'color_0': 'cam_high',
+                         'color_1': 'cam_left_wrist', 'color_2': 'cam_right_wrist'},
+    json_state_data_name=['left_arm', 'right_arm'],
+    json_action_data_name=['left_arm', 'right_arm']
 )
 
 
@@ -53,9 +63,9 @@ Z1_SINGLE_CONFIG = RobotConfig(
         "cam_high",
         "cam_wrist",
     ],
-    camera_to_image_key = {'color_0': 'cam_high', 'color_1': 'cam_wrist'},
-    json_state_data_name = ['left_arm', 'right_arm'],
-    json_action_data_name = ['left_arm', 'right_arm']
+    camera_to_image_key={'color_0': 'cam_high', 'color_1': 'cam_wrist'},
+    json_state_data_name=['left_arm', 'right_arm'],
+    json_action_data_name=['left_arm', 'right_arm']
 )
 
 
@@ -83,10 +93,11 @@ G1_GRIPPER_CONFIG = RobotConfig(
         "cam_right_high",
         "cam_left_wrist",
         "cam_right_wrist",
-        ],
-    camera_to_image_key = {'color_0': 'cam_left_high', 'color_1':'cam_right_high', 'color_2': 'cam_left_wrist' ,'color_3': 'cam_right_wrist'},
-    json_state_data_name = ['left_arm', 'right_arm', 'left_hand', 'right_hand'],
-    json_action_data_name = ['left_arm', 'right_arm', 'left_hand', 'right_hand']
+    ],
+    camera_to_image_key={'color_0': 'cam_left_high', 'color_1': 'cam_right_high',
+                         'color_2': 'cam_left_wrist', 'color_3': 'cam_right_wrist'},
+    json_state_data_name=['left_arm', 'right_arm', 'left_hand', 'right_hand'],
+    json_action_data_name=['left_arm', 'right_arm', 'left_hand', 'right_hand']
 )
 
 
@@ -127,13 +138,14 @@ G1_DEX3_CONFIG = RobotConfig(
         "cam_left_wrist",
         "cam_right_wrist",
     ],
-    camera_to_image_key = {'color_0': 'cam_left_high', 'color_1':'cam_right_high', 'color_2': 'cam_left_wrist' ,'color_3': 'cam_right_wrist'},
-    json_state_data_name = ['left_arm', 'right_arm', 'left_hand', 'right_hand'],
-    json_action_data_name = ['left_arm', 'right_arm', 'left_hand', 'right_hand']
+    camera_to_image_key={'color_0': 'cam_left_high', 'color_1': 'cam_right_high',
+                         'color_2': 'cam_left_wrist', 'color_3': 'cam_right_wrist'},
+    json_state_data_name=['left_arm', 'right_arm', 'left_hand', 'right_hand'],
+    json_action_data_name=['left_arm', 'right_arm', 'left_hand', 'right_hand']
 )
 
 
-G1_INSPIRE_CONFIG = RobotConfig(
+G1_INSPIRE_CONFIG = TactileRobotConfig(
     motors=[
         "kLeftShoulderPitch",
         "kLeftShoulderRoll",
@@ -165,9 +177,81 @@ G1_INSPIRE_CONFIG = RobotConfig(
     cameras=[
         "cam_left_high",
     ],
-    camera_to_image_key = {'color_0': 'cam_left_high'},
-    json_state_data_name = ['left_arm', 'right_arm', 'left_hand', 'right_hand'],
-    json_action_data_name = ['left_arm', 'right_arm', 'left_hand', 'right_hand']
+    tactiles=[
+        "left_tactile_little_finger_tip",  # 3*3
+        "left_tactile_little_finger_nail",  # 12*8
+        "left_tactile_little_finger_pad",  # 10*8
+        "left_tactile_ring_finger_tip",  # 3*3
+        "left_tactile_ring_finger_nail",  # 12*8
+        "left_tactile_ring_finger_pad",  # 10*8
+        "left_tactile_middle_finger_tip",  # 3*3
+        "left_tactile_middle_finger_nail",  # 12*8
+        "left_tactile_middle_finger_pad",  # 10*8
+        "left_tactile_index_finger_tip",  # 3*3
+        "left_tactile_index_finger_nail",  # 12*8
+        "left_tactile_index_finger_pad",  # 10*8
+        "left_tactile_thumb_tip",  # 3*3
+        "left_tactile_thumb_nail",  # 12*8
+        "left_tactile_thumb_middle",  # 3*3
+        "left_tactile_thumb_pad",  # 12*8
+        "left_tactile_palm",  # 8*14
+        "right_tactile_little_finger_tip",  # 3*3
+        "right_tactile_little_finger_nail",  # 12*8
+        "right_tactile_little_finger_pad",  # 10*8
+        "right_tactile_ring_finger_tip",  # 3*3
+        "right_tactile_ring_finger_nail",  # 12*8
+        "right_tactile_ring_finger_pad",  # 10*8
+        "right_tactile_middle_finger_tip",  # 3*3
+        "right_tactile_middle_finger_nail",  # 12*8
+        "right_tactile_middle_finger_pad",  # 10*8
+        "right_tactile_index_finger_tip",  # 3*3
+        "right_tactile_index_finger_nail",  # 12*8
+        "right_tactile_index_finger_pad",  # 10*8
+        "right_tactile_thumb_tip",  # 3*3
+        "right_tactile_thumb_nail",  # 12*8
+        "right_tactile_thumb_middle",  # 3*3
+        "right_tactile_thumb_pad",  # 12*8
+        "right_tactile_palm",  # 8*14
+    ],
+    tactile_to_image_shape=OrderedDict({
+        "left_tactile_little_finger_tip": (3, 3, 3),
+        "left_tactile_little_finger_nail": (3, 12, 8),
+        "left_tactile_little_finger_pad": (3, 10, 8),
+        "left_tactile_ring_finger_tip": (3, 3, 3),
+        "left_tactile_ring_finger_nail": (3, 12, 8),
+        "left_tactile_ring_finger_pad": (3, 10, 8),
+        "left_tactile_middle_finger_tip": (3, 3, 3),
+        "left_tactile_middle_finger_nail": (3, 12, 8),
+        "left_tactile_middle_finger_pad": (3, 10, 8),
+        "left_tactile_index_finger_tip": (3, 3, 3),
+        "left_tactile_index_finger_nail": (3, 12, 8),
+        "left_tactile_index_finger_pad": (3, 10, 8),
+        "left_tactile_thumb_tip": (3, 3, 3),
+        "left_tactile_thumb_nail": (3, 12, 8),
+        "left_tactile_thumb_middle": (3, 3, 3),
+        "left_tactile_thumb_pad": (3, 12, 8),
+        "left_tactile_palm": (3, 8, 14),
+        "right_tactile_little_finger_tip": (3, 3, 3),
+        "right_tactile_little_finger_nail": (3, 12, 8),
+        "right_tactile_little_finger_pad": (3, 10, 8),
+        "right_tactile_ring_finger_tip": (3, 3, 3),
+        "right_tactile_ring_finger_nail": (3, 12, 8),
+        "right_tactile_ring_finger_pad": (3, 10, 8),
+        "right_tactile_middle_finger_tip": (3, 3, 3),
+        "right_tactile_middle_finger_nail": (3, 12, 8),
+        "right_tactile_middle_finger_pad": (3, 10, 8),
+        "right_tactile_index_finger_tip": (3, 3, 3),
+        "right_tactile_index_finger_nail": (3, 12, 8),
+        "right_tactile_index_finger_pad": (3, 10, 8),
+        "right_tactile_thumb_tip": (3, 3, 3),
+        "right_tactile_thumb_nail": (3, 12, 8),
+        "right_tactile_thumb_middle": (3, 3, 3),
+        "right_tactile_thumb_pad": (3, 12, 8),
+        "right_tactile_palm": (3, 8, 14),
+    }),
+    camera_to_image_key={'color_0': 'cam_left_high'},
+    json_state_data_name=['left_arm', 'right_arm', 'left_hand', 'right_hand'],
+    json_action_data_name=['left_arm', 'right_arm', 'left_hand', 'right_hand']
 )
 
 
